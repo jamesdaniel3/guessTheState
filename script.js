00ea3613caa3eea48f2e45baeff6d8ff1dc7f695
jamesdaniel3 <jamesmd333@gmail.com>
2024-01-10 23:26:03 -0500

    added an event listener for the again button that calls the randomState function, will need to also clear textboxes in the future

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const stateEl = document.querySelector(`.state`);
+const againBtn = document.querySelector(`.again`);
+let currentStateNum = 0;
 
 const randomState = function () {
   // currently this starts as one state and quickly switches to the other
   const state_num = Math.trunc(Math.random() * 50);
+  currentStateNum = state_num;
   console.log(state_num);
   stateEl.src = `state${state_num}.png`;
 };
 
 randomState();
+
+againBtn.addEventListener(`click`, function () {
+  console.log("ENTERED");
+  randomState(); // change state
+  // reset all text boxes
+});
